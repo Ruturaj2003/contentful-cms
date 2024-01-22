bb75c4e9051ba6fab7046eacf0ef2bfdff37bc9f
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-22 19:48:50 +0530

    Implement Hook For Fetching Projects Data

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,5 +1,7 @@
-import './fetchProjects';
+import { useFetchProjects } from './fetchProjects';
 const Projects = () => {
+  const { isLoading, projects } = useFetchProjects();
+  console.log(projects);
   return <div>Projects</div>;
 };
 export default Projects;
--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,4 +1,6 @@
 import { createClient } from 'contentful';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const client = createClient({
   space: 'mxg3hx82bt6s',
@@ -6,8 +8,36 @@ const client = createClient({
   accessToken: 'MP3acEgvha-XO88TOZkGRcQTpc0u9gS_m4R7zUhgwdM',
 });
 
-client
-  .getEntries({ content_type: 'projects' })
-  // .getContentType('projects')
-  .then((contentType) => console.log(contentType))
-  .catch(console.error);
+export const useFetchProjects = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: 'projects' });
+
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return {
+          title,
+          url,
+          id,
+          img,
+        };
+      });
+      setProjects(projects);
+
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { isLoading, projects };
+};
